3d806bda2827d8a1866cb6b0ccc73a6bfcb17b6e
SpaceChemist <cleverenz21@gmail.com>
2021-02-22 01:55:28 -0800

    workingish animation

--- JumpDriveInhibitor/animator.cs
+++ JumpDriveInhibitor/animator.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using Sandbox.Definitions;
+using Sandbox.Game.Entities;
+using Sandbox.Game.Entities.Blocks;
 using Sandbox.Game.Entities.Cube;
 using Sandbox.Game.EntityComponents;
 using Sandbox.Game.Lights;
@@ -21,6 +24,8 @@ namespace JumpDriveInhibitor
     [MySessionComponentDescriptor(VRage.Game.Components.MyUpdateOrder.BeforeSimulation)]
     public class Animator : MySessionComponentBase
     {
+        MyParticleEffect effect;
+        MyEntitySubpart subpart;
         private readonly Dictionary<long, IMyCubeGrid> _grids = new Dictionary<long, IMyCubeGrid>();
         private List<IMyBeacon> beaconlist = new List<IMyBeacon>();
         private const string SUBPART_NAME = "Crystal"; // dummy name without the "subpart_" prefix
@@ -30,8 +35,7 @@ namespace JumpDriveInhibitor
         private readonly Vector3 ROTATION_AXIS = Vector3.Up; // rotation axis for the subpart, you can do new Vector3(0.0f, 0.0f, 0.0f) for custom values
         private const float MAX_DISTANCE_SQ = 1000 * 1000; // player camera must be under this distance (squared) to see the subpart spinning
         private int RotationTime { get; set; }
-
-       
+        private bool once { get; set; } = false;
         private bool subpartFirstFind = true;
         private Matrix subpartLocalMatrix; // keeping the matrix here because subparts are being re-created on paint, resetting their orientations
         private float targetSpeedMultiplier; // used for smooth transition
@@ -76,21 +80,38 @@ namespace JumpDriveInhibitor
             var gts = MyAPIGateway.TerminalActionsHelper.GetTerminalSystemForGrid(fuck.CubeGrid);
             gts.GetBlocksOfType(beaconlist,
                 block => { return block.BlockDefinition.SubtypeName.Equals("JumpInhibitor"); });
-            MyAPIGateway.Utilities.ShowNotification("let me die");
-            MyAPIGateway.Utilities.ShowNotification(beaconlist.Count.ToString());
+            //MyAPIGateway.Utilities.ShowNotification("let me die");
+            //MyAPIGateway.Utilities.ShowNotification(beaconlist.Count.ToString());
         }
 
         public override void UpdateBeforeSimulation()
         {
             try
             {
+                
                 foreach (var _beacon in beaconlist)
                 {
                     
+                    var entity = _beacon as MyEntity;
+
                     bool shouldSpin = _beacon.IsWorking; // if block is functional and enabled and powered.
 
-                    if(!shouldSpin && Math.Abs(targetSpeedMultiplier) < 0.00001f)
+                    if (_beacon == null || !_beacon.Enabled || !_beacon.IsWorking ||
+                        !_beacon.IsFunctional)
+                    {
+                        if (entity.TryGetSubpart(SUBPART_NAME, out subpart))
+                        {
+                            subpart.SetEmissiveParts("EmissiveSpotlight", Color.DarkRed, 1.0f);
+                            effect.Stop();
+                            once = false;
+                        }    
+                    }    
+                    
+                    if (!shouldSpin && Math.Abs(targetSpeedMultiplier) < 0.00001f)
+                    {
                         return;
+                    }    
+                        
 
                     if(shouldSpin && targetSpeedMultiplier < 1)
                     {
@@ -107,14 +128,13 @@ namespace JumpDriveInhibitor
                         return;
                     
                     
-                    var g = _beacon.CubeGrid;
-                    var entity = _beacon as MyEntity;
-                    var sink = entity.Components.Get<MyResourceSinkComponent>();
-                    var bp = sink.CurrentInput;
-                    MyAPIGateway.Utilities.ShowNotification(bp.ToString(), 5000);
+                    
+                    
+                 
+                    //MyAPIGateway.Utilities.ShowNotification(bp.ToString(), 5000);
                     
 
-                    MyEntitySubpart subpart;
+                    
                     if(entity.TryGetSubpart(SUBPART_NAME, out subpart)) // subpart does not exist when block is in build stage
                     {
                         //MyAPIGateway.Utilities.ShowNotification(subpart.ToString());
@@ -132,18 +152,28 @@ namespace JumpDriveInhibitor
 
                         subpart.PositionComp.LocalMatrix = subpartLocalMatrix;
                     }
-                    /*entity.SetEmissiveParts("Emissive", Color.White, 0f);
-                    entity.SetEmissiveParts("Emissive", Color.Red, 1000f);
-                    subpart.SetEmissiveParts("Emissive", Color.Red, 1000f);*/
+                    //entity.SetEmissiveParts("Emissive", Color.White, 0f);
+                    //entity.SetEmissiveParts("EmissiveSpotlight", Color.DarkGreen, 1.0f);
+                    subpart.SetEmissiveParts("EmissiveSpotlight", Color.LimeGreen, _beacon.Radius/6000);
                     
+                    if (!once)
+                    {
+                        MyParticlesManager.TryCreateParticleEffect( "ExhaustElectricSmall" , subpartLocalMatrix,  out effect);
+                        
+                        effect.WorldMatrix = _beacon.WorldMatrix;
+                        effect.Play();
+                        once = true;
+                    }
+                    effect.UserScale = _beacon.Radius/6000;
+                    effect.UserEmitterScale = _beacon.Radius/6000;
                     MyEntitySubpart subpart2;
+                    
                     if (subpart.TryGetSubpart("Ring", out subpart2))
                     {
                         RotationTime += 1;
-                        var rotationMatrix = Matrix.CreateRotationY((0.01f * bp/10) * RotationTime);
+                        var rotationMatrix = Matrix.CreateRotationY((_beacon.Radius/6000) * RotationTime);
                         subpart2.PositionComp.SetLocalMatrix(ref rotationMatrix, null, true);
                     }
-                    
                 }
             }
             catch (Exception e)
