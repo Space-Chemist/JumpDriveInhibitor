8df64ab8bfd4a58e1245fa08491f43b68ba1ca94
SpaceChemist <cleverenz21@gmail.com>
2021-02-22 21:00:32 -0800

    storage

---
+++ JumpDriveInhibitor/BeaconStorage.cs
@@ -0,0 +1,14 @@
+﻿using Sandbox.ModAPI;
+using VRage.Game;
+
+namespace JumpDriveInhibitor
+{
+    public class BeaconStorage
+    {
+        public IMyBeacon Beacon { get; set; }
+        
+        public MyParticleEffect Effect { get; set; }
+        
+        public int RingRotation { get; set; }
+    }
+}
